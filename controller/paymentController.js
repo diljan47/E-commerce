5d8affca17a4e582940df1030df24f89d97f00c8
diljan47 <mohddiljan47@gmail.com>
2024-04-02 00:36:46 +0530

    updates

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -1,8 +1,8 @@
 const Razorpay = require("razorpay");
 
 const instance = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_SECRET,
+  key_id: process.env.key_id,
+  key_secret: process.env.key_secret,
 });
 
 const checkOut = async (req, res) => {
