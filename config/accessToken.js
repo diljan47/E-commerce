45f548d56a1d29d08cc3f7010039b865817f7d50
diljan47 <mohddiljan47@gmail.com>
2024-04-01 21:22:41 +0530

    updates

--- config/accessToken.js
+++ config/accessToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const accessToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_TOKEN, { expiresIn: "60m" });
+  return jwt.sign({ id }, process.env.JWT_TOKEN, { expiresIn: "1hr" });
 };
 
 module.exports = { accessToken };
