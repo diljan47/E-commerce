bdc6b20cd95eb6274375c9e4264ee532d89ddb08
diljan47 <mohddiljan47@gmail.com>
2024-04-05 20:58:41 +0530

    update

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
-app.use("/api/user", userRoute);
-app.use("/api/products", productsRoute);
-app.use("/api/category", categoryRoute);
-app.use("/api/color", colorRoute);
-app.use("/api/brand", brandRoute);
+app.use("/user", userRoute);
+app.use("/products", productsRoute);
+app.use("/category", categoryRoute);
+app.use("/color", colorRoute);
+app.use("/brand", brandRoute);
 
 dbConnect().then(() => {
   app.listen(PORT, () => {
